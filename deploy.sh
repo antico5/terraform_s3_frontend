13017f66d6036d06136485e57ab813a0db69b84d
Armando Andini <armando.andini@gmail.com>
2020-10-22 10:48:00 -0300

    Update distribution id

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws s3 cp public/index.html s3://frontend-bucket-3947
-aws cloudfront create-invalidation --distribution-id E1CRLJKIFVDFHK --paths "/*"
+aws cloudfront create-invalidation --distribution-id E3FVSJF41YRQWN --paths "/*"
